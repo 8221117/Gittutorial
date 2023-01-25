84d8d8d1d1a1c496d5445ee507eb61f4bde07a02
8221117 <sainandhu1998@gmail.com>
2023-01-25 15:12:08 +0530

    Added getelementsbytagname

--- app.js
+++ app.js
@@ -76,3 +76,18 @@ var li = document.querySelector(".title");
 li.style.backgroundColor = "Green";
 
 li.style.fontWeight = "bold";
+
+//selectors - GETELEMENTSBYTAGNAME
+var li = document.getElementsByTagName("li");
+
+console.log(li);
+
+console.log(li[1]);
+li[1].textContent = "Hello 2";
+li[1].style.fontWeight = "bold";
+li[2].style.backgroundColor = "Green";
+
+for (i = 0; i < li.length; i++) {
+  //li[i].style.backgroundColor = "yellow";
+  li[i].style.fontWeight = "bold";
+}
