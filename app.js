fde3e34cad9d838390ece446fc768d340cab3b42
8221117 <sainandhu1998@gmail.com>
2023-01-27 20:01:43 +0530

    keywords used to manipulate the DOM

--- app.js
+++ app.js
@@ -140,3 +140,126 @@ var lisecond = document.querySelectorAll("li");
 
 lisecond[1].style.backgroundColor = "white"; //makes second item in li to white background.
 lisecond[1].style.color = "green"; //makes second item in li to green font color.
+
+//traversing the DOM .
+
+var itemList = document.querySelector("#items"); // Selects the ul tag of id = items
+
+//parentnode
+
+//console.log(itemList.parentNode); // Gives the parent of the UL which is div here
+
+//itemList.parentNode.style.backgroundColor = "yellow"; // changes the parent node (div with id main)to yellow.
+
+//console.log(itemList.parentNode.parentNode); // gives the parent of div with id main as div with container class
+
+// console.log(itemList.parentNode.parentNode.parentNode); // gives the body which is parent element of div class container.
+
+//Parentelement (same as parent node)
+
+console.log(itemList.parentElement); // Gives the parent of the UL which is div here
+
+itemList.parentElement.style.backgroundColor = "yellow"; // changes the parent node (div with id main)to yellow.
+
+console.log(itemList.parentElement.parentElement); // gives the parent of div with id main as div with container class
+
+console.log(itemList.parentElement.parentElement.parentElement); // gives the body which is parent element of div class container.
+
+//childnodes
+
+//console.log(itemList.childNodes); //gIves all the  childs of ul . ie li but with linebreak as another text nodes .
+
+//children
+
+console.log(itemList.children); // gives all the child of ul ie li
+console.log(itemList.children[1]);
+itemList.children[3].style.backgroundColor = "orange";
+
+//Firstchild (more or less same like child nodes )
+
+console.log(itemList.firstChild); //gives the first text node (extra line break ) like child node . if we remove the linebreak before the li 1 , then it will give li 1.
+
+//firstelementchild
+
+console.log(itemList.firstElementChild); // gives the first li , as line breaks are not considered here like firstChild.
+
+itemList.firstElementChild.textContent = "Boo";
+
+//Lastchild (more or less same like child nodes )
+
+console.log(itemList.lastChild); //gives the last text node (extra line break ) like child node . if we remove the linebreak before the li 1 , then it will give li 1.
+
+//lastelementchild
+
+console.log(itemList.lastElementChild); // gives the last li , as line breaks are not considered here like last Child.
+
+itemList.lastElementChild.textContent = "Boo boo";
+
+//nextsibling
+
+console.log(itemList.nextSibling); //gives the text line break element as sibling
+
+//nextElementSibling
+
+console.log(itemList.nextElementSibling); // gives span tag .
+
+//previoussibling
+console.log(itemList.previousSibling);
+
+//previousElementSibling
+
+console.log(itemList.previousElementSibling); // Gives h2 element which is above the ul .
+
+itemList.previousElementSibling.style.backgroundColor = "blue";
+
+//createElement
+
+var newDiv = document.createElement("div");
+//add class
+newDiv.className = "sam";
+//add id
+newDiv.id = "nandhu";
+
+// add attribute
+newDiv.setAttribute("title", "hello div");
+
+console.log(newDiv);
+
+//create text node
+
+var newdivText = document.createTextNode("hello world");
+
+// appendchild - add text to div
+newDiv.style.fontSize = "30px";
+newDiv.appendChild(newdivText); // now it is javascript . this we need to insert into the dom .
+console.log(newDiv);
+
+//inserting the div element into the DOM
+
+// Now we can put it below header of div class container between line no 17 and 18 in html .
+var container = document.querySelector("header .container");
+
+var h1 = document.querySelector("header h1");
+
+container.insertBefore(newDiv, h1); // adds newDiv before h1 in the header div class container .
+
+//createElement
+var para = document.createElement("p");
+
+para.className = "bujju";
+
+para.setAttribute("title", "hello para");
+
+//create text node
+
+var newParatext = document.createTextNode("Hello ");
+
+para.appendChild(newParatext);
+
+console.log(para);
+
+var container2 = document.querySelector("div .list-group");
+
+var li1 = document.querySelector("div .list-group-item");
+
+container2.insertBefore(para, li1);
