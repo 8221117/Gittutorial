7bca1b5d48c46aa681ec86d39298633fe4f36171
8221117 <sainandhu1998@gmail.com>
2023-01-28 15:59:09 +0530

    Add elements to the list . Add delete and edit button .

---
+++ app2.js
@@ -0,0 +1,84 @@
+var form = document.getElementById("addForm");
+
+var itemList = document.getElementById("items");
+
+//form submit event
+
+form.addEventListener("submit", addItem);
+
+//del event
+
+itemList.addEventListener("click", removeItem);
+
+//add item function
+
+function addItem(e) {
+  e.preventDefault();
+  // console.log(1);
+
+  //get the input value for the form input type=text
+
+  var newItem = document.getElementById("item").value;
+
+  //create new li element
+
+  var li = document.createElement("li");
+
+  // add class name to the li
+
+  li.className = "list-group-item";
+
+  //console.log(li);
+
+  // add text node with input value into the li from the add form element (submit button)
+
+  li.appendChild(document.createTextNode(newItem));
+
+  //create delete button element
+
+  var delBtn = document.createElement("button");
+
+  // add class name to the delete button
+
+  delBtn.className = "btn btn-danger btn-sm float-right delete";
+
+  //add text node to the delete button with value x ;
+
+  delBtn.appendChild(document.createTextNode("x"));
+
+  // add the delete button to the li
+
+  li.appendChild(delBtn);
+
+  // create edit button
+
+  var editBtn = document.createElement("button");
+
+  //add text node to the edit button with value edit ;
+
+  editBtn.appendChild(document.createTextNode("Edit"));
+
+  // add class name to the edit button
+
+  editBtn.className = "btn btn-primary btn-sm float-right edit";
+
+  // add the edit button to the li
+
+  li.appendChild(editBtn);
+
+  // add li to Ul
+
+  itemList.appendChild(li);
+}
+
+// remove li Item function
+
+function removeItem(e) {
+  if (e.target.classList.contains("delete")) {
+    //console.log(1);
+    if (confirm("Are you sure?")) {
+      var li = e.target.parentElement; // li is the parent element of del button
+      itemList.removeChild(li); //ul (itemList) is the parent of li.
+    }
+  }
+}
